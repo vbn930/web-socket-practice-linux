2791d2eeb518f2ed59fb1bddbfd3cef052979e96
vbn930 <vbn930@naver.com>
2025-06-12 21:42:02 +0900

    Add TCP socket example

---
+++ 2-1/tcp_client.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+int main(int argc, char *argv[]){
+    int sock;
+    struct sockaddr_in serv_addr;
+    char message[30];
+    int str_len = 0;
+
+    int idx = 0, read_len = 0;
+
+    if(argc != 3){
+        std::cout << "Usage : " << argv[0] << " <IP> <port>\n";
+        exit(1);
+    }
+
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1){
+        std::cerr << "socket() error\n";
+        exit(1);
+    }
+    
+    serv_addr = sockaddr_in{};
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if(connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1){
+        std::cerr << "connect() error\n";
+        exit(1);
+    }
+
+    while(read_len = read(sock, &message[idx++], 1)){
+        if(str_len == -1){
+            std::cerr << "read() error\n";
+            exit(1);
+        }
+
+        str_len += read_len;
+    }
+
+    std::cout << "Message for server: " << message << "\n";
+    std::cout << "Function read call count: " << str_len << "\n";
+    close(sock);
+    return 0;
+}
---
+++ 2-1/tcp_server.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+int main(int argc, char *argv[]){
+    int serv_sock;
+    int clnt_sock;
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    char message[] = "Hello World";
+
+    if(argc != 2){
+        std::cout << "Usage : " << argv[0] << " <port>\n";
+        exit(1);
+    }
+
+    //socket 함수는 소켓을 생성 후 해당 소켓에 대한 파일 디스럽터를 반환한다 (int 값).
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(serv_sock == -1){
+        std::cerr << "socket() error\n";
+        exit(1);
+    }
+    
+    serv_addr = sockaddr_in{};
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1){
+        std::cerr << "bind() error\n";
+        exit(1);
+    }
+
+    if(listen(serv_sock, 5)==-1){
+        std::cerr << "listen() error\n";
+        exit(1);
+    }
+
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_addr, &clnt_addr_size);
+    if(clnt_sock == -1){
+        std::cerr << "accept() error\n";
+        exit(1);
+    }
+
+    //write 함수는 파일에 데이터를 출력 (소켓의 경우엔 전송) 하는 함수이다 -> 파일과 소켓에서 모두 사용한다.
+    write(clnt_sock, message, sizeof(message));
+
+    //close 함수는 파일, 소켓을 닫을때 공통적으로 사용한다 -> 파일과 소켓을 구분하지 않는 리눅스의 특성 때문이다.
+    close(clnt_sock);
+    close(serv_sock);
+    return 0;
+}
