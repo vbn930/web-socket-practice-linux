cd3690a59fff9f5128274a108fb287203a1484df
vbn930 <vbn930@naver.com>
2025-06-15 16:28:14 +0900

    Add echo server example

---
+++ 4-3/echo_client.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 1024
+void error_handling(const char* message);
+
+int main(int argc, char* argv[]){
+    int sock;
+    char message[BUF_SIZE];
+    int str_len = 0;
+    sockaddr_in sock_addr;
+
+    if(argc != 3){
+        std::cout << "Usage: " << argv[0] << " <IP> <PORT>\n";
+        exit(1);
+    }
+    
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock == -1){
+        error_handling("sock()");
+    }
+
+    sock_addr = sockaddr_in{};
+    sock_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    sock_addr.sin_family = AF_INET;
+    sock_addr.sin_port = htons(atoi(argv[2]));
+
+    if(connect(sock, (sockaddr*) &sock_addr, sizeof(sock_addr)) == -1){
+        error_handling("connect()");
+    }
+
+    while(true){
+        std::cout << "Input message (Q to quit): ";
+        std::cin >> message;
+        
+        if(!strcmp(message, "q") || !strcmp(message, "Q")){
+            break;
+        }
+
+        write(sock, message, strlen(message));
+        str_len = read(sock, message, BUF_SIZE-1);
+        message[str_len] = 0;
+        std::cout << "Message from server: " << message << "\n";
+    }
+    close(sock);
+    return 0;
+}
+
+void error_handling(const char* message){
+    std::cerr << message << " error!" << std::endl;
+    exit(1);
+}
